c6a3ac0ae1a7b3683d1a0287546b29cbe4657f74
derekcheungsa <122410431+derekcheungsa@users.noreply.github.com>
2023-07-12 22:30:36 -0400

    update

--- app/main.py
+++ app/main.py
@@ -1,28 +1,17 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-
-class Item(BaseModel):
-    name: str
-    description: str = None
-    price: float
-    is_offer: bool = None
-
-# assuming this is our "database"
-items_db = {
-    "1": {"name": "item1", "description": "This is item 1", "price": 25.0, "is_offer": True},
-    "2": {"name": "item2", "description": "This is item 2", "price": 45.0, "is_offer": False},
-}
+from langchain.retrievers import WikipediaRetriever
 
 app = FastAPI()
 
-class ItemID(BaseModel):
-    id: str
+class Query(BaseModel):
+    query: str
 
-@app.post("/items/")
-async def retrieve_item(item_id: ItemID):
-    item_id_dict = item_id.dict()
-    id = item_id_dict['id']
-    if id in items_db:
-        return {"message": f"Item retrieved: {items_db[id]['name']}", "data": items_db[id]}
-    else:
-        raise HTTPException(status_code=404, detail="Item not found")
+app = FastAPI()
+
+@app.post("/query/")
+async def process_query(query: Query):
+    retriever = WikipediaRetriever()
+    docs = retriever.get_relevant_documents(query=query)
+    response = docs[0].page_content[:400]
+    return {"reply": response }
